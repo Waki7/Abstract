a9d9227dc7451651caedea4b6626da726e009896
Waki7 <c.waki7@gmail.com>
2020-07-17 00:42:50 -0400

    attempt at experiment with mobilenet again, set device num

--- settings.py
+++ settings.py
@@ -1,19 +1,20 @@
+import logging
 import random
 
 import numpy as np
 import torch
-
-configs = {
-
-}
 
 LOG_DIR = 'logs'
 ENCODER_WEIGHTS = 'networks/weights'
 
 grads = {}
 if torch.cuda.is_available():
+    logging.info('using cpu')
     DEVICE = torch.device('cuda')
 else:
+    DEVICE_NUM = 0
+    logging.info('{} gpus available, will be using gpu {}'.format(torch.cuda.device_count(), DEVICE_NUM))
+    torch.cuda.set_device(DEVICE_NUM)
     DEVICE = torch.device('cpu')
 
 DTYPE_LONG = torch.long
