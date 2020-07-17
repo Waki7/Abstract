fa4ea14396a46b3e90bc6d0313796eca8525c054
Waki7 <c.waki7@gmail.com>
2020-07-17 00:28:32 -0400

    stubs for training protocools, why am i bothering with openai gym vs init myself wtf

--- grid_world/envs_encoder_training/maneuver_simple_training.py
+++ grid_world/envs_encoder_training/maneuver_simple_training.py
@@ -1,3 +1,15 @@
+import gym
+import grid_world.envs.maneuver_simple as grid_world
+
 class StateEncodingProtocol():
     def __init__(self):
+        self.env: grid_world.ManeuverSimple = gym.make('Grid-v0')
+
+    def get_in_shapes(self):
         pass
+    
+    def get_out_shapes(self):
+        pass
+
+    def generate_batch(self):
+        self.env.reset()
